2dbc3ae55eedcdfea00cfcbcf8eef4a600a27ad1
Mark Edmondson <github@markedmondson.me>
2022-08-26 15:57:01 +0200

    comments

--- admin/image-figs.R
+++ admin/image-figs.R
@@ -1,12 +1,17 @@
+# replace with the folder where your .asciidoc files are
 book_folder <- "../learning-google-analytics/"
 
+# list the .asciidoc files
 chapters <- list.files(book_folder,pattern = "asciidoc$", full.names = TRUE)
 
+# read in the chapters
 book_text <- lapply(chapters, readLines)
 names(book_text) <- basename(chapters)
 
+# parse out only the image:: lines
 book_images <- lapply(book_text, \(x) x[grepl("^image", x)])
 
+# create a list of dfs with image info
 images_df <- lapply(names(book_images), \(x){
   o <- book_images[[x]]
   if(length(o) == 0) return(NULL)
@@ -20,5 +25,8 @@ images_df <- lapply(names(book_images), \(x){
   the_df
 })
 
+# turn it into one data.frame and write to csv
 all_images <- Reduce(rbind, images_df)
 write.csv(all_images, file = "figure-log.csv", row.names = FALSE)
+
+# I then imported the CSV into GoogleSheets for review
